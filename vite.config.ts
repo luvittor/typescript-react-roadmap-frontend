5471685c44467489bec2e3a22f216458c58b53e7
Luciano Vettoretti <luciano.vittoretti@gmail.com>
2025-06-09 11:52:57 -0300

    adjusting aditional settings in vite config and docker-compose to fix hot reload in the container

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    watch: {
-      // force polling every 100ms
-      usePolling: true,
-      interval: 100,
-    },
-  },
 })
